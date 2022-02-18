9a37d573ab3eae7b5c615a495570c49534c8ac2e
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-02-18 18:10:54 +0700

    :heavy_plus_sign: setup firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false
-};
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -14,3 +6,37 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBN1OjGV_lpi4XMVm4tQUBR2w9iJUVguxo",
+  authDomain: "kirirom-forum-chatbot.firebaseapp.com",
+  databaseURL: "https://kirirom-forum-chatbot-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "kirirom-forum-chatbot",
+  storageBucket: "kirirom-forum-chatbot.appspot.com",
+  messagingSenderId: "1047652526437",
+  appId: "1:1047652526437:web:94d67691d74f8b5284d98c",
+  measurementId: "G-FZM9MWH2RB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBN1OjGV_lpi4XMVm4tQUBR2w9iJUVguxo",
+    authDomain: "kirirom-forum-chatbot.firebaseapp.com",
+    databaseURL: "https://kirirom-forum-chatbot-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kirirom-forum-chatbot",
+    storageBucket: "kirirom-forum-chatbot.appspot.com",
+    messagingSenderId: "1047652526437",
+  }
+};
