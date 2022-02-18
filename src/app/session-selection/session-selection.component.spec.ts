01e7913f6d730a32ebdf4a23ace8dea5bd028917
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-02-18 13:09:42 +0700

    :tada: add angular material

---
+++ src/app/session-selection/session-selection.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SessionSelectionComponent } from './session-selection.component';
+
+describe('SessionSelectionComponent', () => {
+  let component: SessionSelectionComponent;
+  let fixture: ComponentFixture<SessionSelectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SessionSelectionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SessionSelectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/session-selection/session-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-session-selection',
+  templateUrl: './session-selection.component.html',
+  styleUrls: ['./session-selection.component.scss']
+})
+export class SessionSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
